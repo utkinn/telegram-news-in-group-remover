a2593b8598aa7bbf654a065096c71031a80151e6
Nikita Utkin <utkin@mindbox.cloud>
2023-09-28 20:13:11 +0500

    It's working ;)

---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	updateConfig := tgbotapi.NewUpdate(0)
+
+	updateConfig.Timeout = 30
+
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		message := update.Message
+		if message == nil {
+			continue
+		}
+
+		fmt.Printf("%+v\n", message.Chat)
+
+		if message.IsCommand() && message.Chat.Type == "private" {
+			switch message.Command() {
+			case "start":
+				sendHelp(bot, message)
+			case "list":
+				listBannedChannels(bot, message)
+			case "unban":
+				// TODO
+			default:
+				sendUnknownCommandResponse(bot, message)
+			}
+		} else {
+			forwardChat := message.ForwardFromChat
+			if forwardChat == nil {
+				continue
+			}
+
+			log.Printf("Got forward from channel %+v\n", message.ForwardFromChat)
+
+			if message.Chat.Type == "private" {
+				channelRecord := channel{Id: message.ForwardFromChat.ID, Title: message.ForwardFromChat.Title}
+				banChannel(channelRecord)
+				sendBanResponse(bot, message)
+			} else {
+				if isChannelIdBanned(forwardChat.ID) {
+					removeMessage(bot, update.Message)
+					mockSender(bot, update.Message)
+				}
+			}
+
+		}
+	}
+}
+
+func listBannedChannels(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	bannedChannels := getBannedChannels()
+	text := listChannelsToString(bannedChannels)
+	response := tgbotapi.NewMessage(message.Chat.ID, text)
+	response.ParseMode = "markdown"
+	sendWithErrorLogging(bot, response)
+}
+
+func listChannelsToString(bannedChannels []channel) string {
+	if len(bannedChannels) == 0 {
+		return "_Каналов нет_"
+	}
+	lines := make([]any, len(bannedChannels))
+	for i := 0; i < len(bannedChannels); i++ {
+		lines[i] = fmt.Sprintf("%d. %s\n", i+1, bannedChannels[i].Title)
+	}
+	return fmt.Sprint(lines...)
+}
+
+func sendUnknownCommandResponse(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	text := fmt.Sprintf("*Неизвестная команда: `%s`*", message.Command())
+	response := tgbotapi.NewMessage(message.Chat.ID, text)
+	response.ParseMode = "markdown"
+	sendWithErrorLogging(bot, response)
+}
+
+func sendBanResponse(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	text := fmt.Sprintf("Канал *%s* забанен.", message.ForwardFromChat.Title)
+	response := tgbotapi.NewMessage(message.Chat.ID, text)
+	response.ParseMode = "markdown"
+	sendWithErrorLogging(bot, response)
+}
+
+func sendHelp(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	sendWithErrorLogging(
+		bot,
+		tgbotapi.NewMessage(
+			message.Chat.ID,
+			"Этот бот удаляет сообщения, пересланные из забаненных вами каналов.\n\nДля того, чтобы забанить канал, перешлите из него сообщение сюда.\nЧтобы разбанить канал, воспользуйтесь /unban.\nПосмотреть список забаненных каналов — /list.",
+		),
+	)
+}
+
+func removeMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	sendWithErrorLogging(bot, tgbotapi.NewDeleteMessage(message.Chat.ID, message.MessageID))
+}
+
+func sendWithErrorLogging(bot *tgbotapi.BotAPI, c tgbotapi.Chattable) {
+	if _, err := bot.Send(c); err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func mockSender(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	text := fmt.Sprintf("%s, атата!", message.From.FirstName)
+	response := tgbotapi.NewMessage(message.Chat.ID, text)
+	sendWithErrorLogging(bot, response)
+}
+
+type channel struct {
+	Id    int64
+	Title string
+}
+
+var bannedChannels []channel
+
+func getBannedChannels() []channel {
+	return bannedChannels
+}
+
+func banChannel(ch channel) {
+	bannedChannels = removeDuplicateChannels(append(bannedChannels, ch))
+}
+
+func removeDuplicateChannels(sliceList []channel) []channel {
+	allKeys := make(map[channel]bool)
+	list := []channel{}
+	for _, item := range sliceList {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
+
+func isChannelIdBanned(channelId int64) bool {
+	for _, ch := range getBannedChannels() {
+		if ch.Id == channelId {
+			return true
+		}
+	}
+	return false
+}
