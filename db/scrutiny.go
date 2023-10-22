10c875827ed921f0abc10c19dcb63877b80928d2
Nikita Utkin <utkin@mindbox.cloud>
2023-10-22 19:16:20 +0500

    /{,un}scrutiny: normalize nick

--- db/scrutiny.go
+++ db/scrutiny.go
@@ -1,17 +1,24 @@
 package db
 
+import "strings"
+
 var scrutinyDb = database[string]{
 	filename: "scrutiny.json",
 }
 
 func AddToScrutiny(nick string) {
-	scrutinyDb.add(nick)
+	scrutinyDb.add(normalizeNick(nick))
 }
 
 func RemoveFromScrutiny(nick string) bool {
+	nick = normalizeNick(nick)
 	return scrutinyDb.removeByCallback(func(n string) bool { return n != nick })
 }
 
 func IsUnderScrutiny(nick string) bool {
 	return scrutinyDb.anyByCallback(func(n string) bool { return n == nick })
+}
+
+func normalizeNick(nick string) string {
+	return strings.Replace(nick, "@", "", 1)
 }
