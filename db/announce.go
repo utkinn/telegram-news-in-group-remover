81ca08e7698c73e19fea0319362ff9729b275024
Nikita Utkin <utkin@mindbox.cloud>
2023-10-23 10:32:23 +0500

    Export announcementSubscription fields

--- db/announce.go
+++ db/announce.go
@@ -1,8 +1,8 @@
 package db
 
 type announcementSubscription struct {
-	userName string
-	chatId   int64
+	UserName string
+	ChatId   int64
 }
 
 var announcementsDb = database[announcementSubscription]{filename: "announcement-subscriptions.json"}
@@ -10,18 +10,18 @@ var announcementsDb = database[announcementSubscription]{filename: "announcement
 func GetChatIdsOfAdminsSubscribedToAnnouncements() []int64 {
 	ids := make([]int64, len(announcementsDb.data))
 	for i, sub := range announcementsDb.data {
-		ids[i] = sub.chatId
+		ids[i] = sub.ChatId
 	}
 	return ids
 }
 
 func SubscribeToAnnouncements(chatId int64, userName string) {
 	announcementsDb.add(announcementSubscription{
-		userName: userName,
-		chatId:   chatId,
+		UserName: userName,
+		ChatId:   chatId,
 	})
 }
 
 func UnsubscribeFromAnnouncements(chatId int64) {
-	announcementsDb.filterInPlaceAndWrite(func(ann announcementSubscription) bool { return ann.chatId != chatId })
+	announcementsDb.filterInPlaceAndWrite(func(ann announcementSubscription) bool { return ann.ChatId != chatId })
 }
