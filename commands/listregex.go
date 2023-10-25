6d935ea7b3c0fc09b18e0f2070539e41c9fd90a7
Nikita Utkin <utkin@mindbox.cloud>
2023-10-25 23:47:04 +0500

    /listregex: send "no regexes" instead of nothing

--- commands/listregex.go
+++ commands/listregex.go
@@ -9,6 +9,10 @@ import (
 
 var listRegexCommand = newCommand("listregex", "Пристальный присмотр - показать список запрещенных регулярных выражений", func(ctx helpers.ResponseContext) {
 	regexes := db.GetRegexes()
+	if len(regexes) == 0 {
+		ctx.SendSilentMarkdownFmt("_Нет регулярных выражений_")
+		return
+	}
 	regexesOrderedListLines := make([]string, len(regexes))
 	for i, regex := range regexes {
 		regexesOrderedListLines[i] = fmt.Sprintf("%d. %s", i+1, regex)
