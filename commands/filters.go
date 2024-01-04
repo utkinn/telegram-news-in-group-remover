utkinn/telegram-news-in-group-remover

commit 738e55bb1b47e7d49c7ea7818b9767837e378bc3

Make text of some commands more helpful

commands/filters.go
@@ -29,7 +29,7 @@ func init() {
 					scrutinyNotice = "\n      ▪️_Только для пристального присмотра (/scrutiny)_"
 				}
 
-				lines[i] = fmt.Sprintf("%v `[%v]` %v\n      %v%v", stateEmoji, desc.ID, desc.Name, desc.Desc, scrutinyNotice)
+				lines[i] = fmt.Sprintf("%v [`%v`] %v\n      %v%v", stateEmoji, desc.ID, desc.Name, desc.Desc, scrutinyNotice)
 			}
 			ctx.SendSilentMarkdownFmt(strings.Join(lines, "\n\n"))
 		}),

commands/list.go
@@ -8,9 +8,11 @@ import (
 )
 
 func init() {
+	const help = "\n\n_Канал можно забанить, переслав любое сообщение из него мне._"
+
 	registerCommand(
 		newCommand("list", "Список забаненных каналов", func(ctx helpers.ResponseContext) {
-			ctx.SendSilentFmt(listChannelsToString(db.GetBannedChannels()))
+			ctx.SendSilentFmt(listChannelsToString(db.GetBannedChannels()) + help)
 		}),
 	)
 }

commands/listregex.go
@@ -9,18 +9,20 @@ import (
 )
 
 func init() {
+	const help = "\n\n_Добавить регулярное выражение можно командой /banregex._"
+
 	registerCommand(
 		newCommand("listregex", "Пристальный присмотр - показать список запрещенных регулярных выражений", func(ctx helpers.ResponseContext) {
 			regexes := db.GetRegexes()
 			if len(regexes) == 0 {
-				ctx.SendSilentMarkdownFmt("_Нет регулярных выражений_")
+				ctx.SendSilentMarkdownFmt("_Нет регулярных выражений_" + help)
 				return
 			}
 			regexesOrderedListLines := make([]string, len(regexes))
 			for i, regex := range regexes {
 				regexesOrderedListLines[i] = fmt.Sprintf("%d. %s", i+1, regex)
 			}
-			ctx.SendSilentFmt(strings.Join(regexesOrderedListLines, "\n"))
+			ctx.SendSilentFmt(strings.Join(regexesOrderedListLines, "\n") + help)
 		}),
 	)
 }

commands/banregex.go
@@ -10,7 +10,7 @@ func init() {
 		newCommand("banregex", "Пристальный присмотр - запретить сообщения, совпадающие по регулярному выражению", func(ctx helpers.ResponseContext) {
 			regex := ctx.Message.CommandArguments()
 			if len(regex) == 0 {
-				ctx.SendSilentMarkdownFmt("Ты забыл ввести регулярное выражение.\nСправка [тут](https://golang-blog.blogspot.com/2020/03/regexp-golang.html). Подебажить выражение можешь [тут](https://regex101.com).")
+				ctx.SendSilentMarkdownFmt("Ты забыл ввести регулярное выражение.\n[Тут](https://golang-blog.blogspot.com/2020/03/regexp-golang.html) можно узнать, что это такое. А [тут](https://regex101.com) можно его потестировать.")
 				return
 			}
 

commands/getupdatenews.go
@@ -9,7 +9,7 @@ func init() {
 	registerCommand(
 		newCommand("getupdatenews", "Подписаться на новости об обновлениях этого бота", func(ctx helpers.ResponseContext) {
 			db.SubscribeToAnnouncements(ctx.Message.Chat.ID, ctx.Message.From.UserName)
-			ctx.SendSilentMarkdownFmt("Ты подписан на новости об обновлениях.")
+			ctx.SendSilentMarkdownFmt("Ты подписан на новости об обновлениях. Отписаться можно командой /noupdatenews.")
 		}),
 	)
 }

commands/scrutiny.go
@@ -16,7 +16,7 @@ func init() {
 				return
 			}
 			db.AddToScrutiny(userName)
-			ctx.SendSilentMarkdownFmt("*%s* теперь под _пристальным присмотром_.", userName)
+			ctx.SendSilentMarkdownFmt("*%s* теперь под _пристальным присмотром_. Вытащить его оттуда можно командой /unscrutiny.", userName)
 		}),
 	)
 }

commands/stfu.go
@@ -20,7 +20,7 @@ func init() {
 				}
 
 				db.MuteUser(userName)
-				ctx.SendSilentMarkdownFmt("Пользователь с ником %s отправлен на принудительный отдых.", userName)
+				ctx.SendSilentMarkdownFmt("Пользователь с ником %s отправлен на принудительный отдых. Если захочется скостить срок — используй /unstfu.", userName)
 			},
 		),
 	)

commit ae414a67a6012e08cd09c55daee3976c11ad093a

Fix markup issues introduced in 738e55bb1

commands/filters.go
@@ -29,7 +29,7 @@ func init() {
 					scrutinyNotice = "\n      ▪️_Только для пристального присмотра (/scrutiny)_"
 				}
 
-				lines[i] = fmt.Sprintf("%v [`%v`] %v\n      %v%v", stateEmoji, desc.ID, desc.Name, desc.Desc, scrutinyNotice)
+				lines[i] = fmt.Sprintf("%v \\[ `%v` ] %v\n      %v%v", stateEmoji, desc.ID, desc.Name, desc.Desc, scrutinyNotice)
 			}
 			ctx.SendSilentMarkdownFmt(strings.Join(lines, "\n\n"))
 		}),

commands/list.go
@@ -8,7 +8,7 @@ import (
 )
 
 func init() {
-	const help = "\n\n_Канал можно забанить, переслав любое сообщение из него мне._"
+	const help = "\n\nКанал можно забанить, переслав любое сообщение из него мне."
 
 	registerCommand(
 		newCommand("list", "Список забаненных каналов", func(ctx helpers.ResponseContext) {

commands/listregex.go
@@ -9,7 +9,7 @@ import (
 )
 
 func init() {
-	const help = "\n\n_Добавить регулярное выражение можно командой /banregex._"
+	const help = "\n\nДобавить регулярное выражение можно командой /banregex."
 
 	registerCommand(
 		newCommand("listregex", "Пристальный присмотр - показать список запрещенных регулярных выражений", func(ctx helpers.ResponseContext) {